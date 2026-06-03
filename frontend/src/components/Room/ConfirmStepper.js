02bffbc9489cd143538ddb2c8af0f853280e5c5c
= <=>
2026-06-03 18:25:34 +0200

    fix limit problem followed artists

--- frontend/src/components/Room/ConfirmStepper.js
+++ frontend/src/components/Room/ConfirmStepper.js
@@ -108,7 +108,7 @@ export default function ConfirmStepper({
                                 <Typography variant="body1" className={'endPage-Stepper-body'} sx={{ color: 'var(--color-tu-berlin)' }}>
                                     {language === "de" ?
                                         "Bitte laden Sie die Seite nicht neu, schließen Sie das Fenster nicht und drücken Sie nicht den Zurück-Button ihres Browsers!" :
-                                        "Please do not refresh the page, close the window, and do not press the back button of your browser!"}
+                                        "Please do not refresh the page, do not close the window, and do not press the back button of your browser!"}
                                 </Typography>
                             </Box>
                             <Box sx={{ textAlign: "center" }}>
